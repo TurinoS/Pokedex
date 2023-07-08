16533014fe21f02a915df7891583f997f759c100
Paulo Turino <paulo.turino01@gmail.com>
2023-07-08 15:17:01 -0300

    feat: iniciando estilização do modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,19 +1,48 @@
 import Image from "next/image";
 import styled from "styled-components";
+import { useContext } from "react";
+import { ApiContext } from "@/context/ContextApi";
+import { ModalContext } from "@/context/ModalContext";
 
 const StyledModal = styled.section`
-    background-color: var(--orange);
-    width: 60vw;
-    height: 60vh;
-    position: absolute;
-    top: 20vh;
-    left: 20vw;
+    background-color: var(--bg-dark);
+    color: var(--font-light);
+    width: 70vw;
+    height: 70vh;
+    position: sticky;
+    top: 10vh;
+    left: 10vw;
+    border: 2px solid var(--orange);
+    border-radius: var(--border-radius);
+
+    & img {
+        border-bottom: 2px solid var(--orange);
+        border-right: 2px solid var(--orange);
+        border-radius: var(--border-radius) 0;
+        background-color: var(--font-dark);
+    }
+
+    & h2 {
+        color: var(--orange);
+        text-transform: capitalize;
+        font-size: 30px;
+        padding-left: 3em;
+    }
 `
 
 export default function Modal() {
+    const { eachPokemonData } = useContext(ApiContext)
+  const { modalOpen, toggleModal } = useContext(ModalContext)
+
+  function openModal() {
+    toggleModal();
+    console.log(modalOpen)
+  }
+
     return(
         <StyledModal>
-            <h2>nome do pokemon</h2>
+            <Image src={eachPokemonData[0].sprites.front_default} alt={eachPokemonData[0].name} width={200} height={200} />
+            <h2>{eachPokemonData[0].name}</h2>
         </StyledModal>
     )
 }
